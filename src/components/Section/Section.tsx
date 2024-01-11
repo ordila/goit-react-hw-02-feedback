a3fb0eae3f947c24ad076a2bcd761f02ae1b88e5
ordila <ordila99@gmail.com>
2024-01-11 23:43:07 +0200

    button ts

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import { FC } from 'react';
 import { ISectionProps } from './Section.types';
 
-export class Section extends Component<ISectionProps> {
-  render() {
-    const { title, children } = this.props;
-    return (
-      <>
-        <h2>{title}</h2>
-        <div>{children}</div>
-      </>
-    );
-  }
-}
+export const Section: FC<ISectionProps> = ({ title, children }) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      <div>{children}</div>
+    </>
+  );
+};
